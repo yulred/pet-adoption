2c97fd727a7a55b23d0bd92a83f5e605f53c5662
Julia <103965884+yulred@users.noreply.github.com>
2022-09-05 16:46:39 +0300

    Add heroku url

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const telRegExpStr = "^((\\+[1-9]{1,4}[\\-]*)|(\\([0-9]{2,3}\\)[\\-]*)|([0-9]{2,
 const cookieSettings = { maxAge: 1209600000, httpOnly: false, overwrite: true };
 
 const dev = { API_URL: "http://localhost:3000" };
-const prod = { API_URL: "" };
+const prod = { API_URL: "https://tranquil-everglades-31307.herokuapp.com/" };
 
 const config = process.env.NODE_ENV === "development" ? dev : prod;
 
