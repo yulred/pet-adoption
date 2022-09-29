1b69633eb134937a94ab08d12f53476a7a35efee
Julia <103965884+yulred@users.noreply.github.com>
2022-09-29 09:20:54 +0300

    Updated readme, config

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Built as a graduation project for the Full-Stack Development programme at Israel
 
 Deployed via Netlify (front-end) and Render (back-end[^1]): https://yulred-pet-adoption.netlify.app
 
-[^1]: May have a response delay of up to 30 seconds for the first request that comes in after a period of inactivity.
+[^1]: May have a response delay of up to 30 seconds due to free tier restrictions.
 
 # Tech Stack
 React, TypeScript (front-end), Chakra UI, Node.js (Express), MongoDB (Mongoose)
--- server/config.js
+++ server/config.js
@@ -2,6 +2,6 @@ const emailRegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 const telRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 const telRegExpStr = "^((\\+[1-9]{1,4}[\\-]*)|(\\([0-9]{2,3}\\)[\\-]*)|([0-9]{2,4})[\\-]*)*?[0-9]{3,4}?[\\-]*[0-9]{3,4}?$";
 
-const cookieSettings = { maxAge: 1209600000, httpOnly: true, overwrite: true };
+const cookieSettings = { maxAge: 1209600000, httpOnly: true, sameSite: "lax", overwrite: true };
 
 module.exports = { emailRegExp, telRegExp, telRegExpStr, cookieSettings };
