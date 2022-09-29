b349e6d27b49637a5531f9804c0af928ff03bbc7
Julia <103965884+yulred@users.noreply.github.com>
2022-09-29 15:12:25 +0300

    Fixed logout

--- server/controllers/logoutController.js
+++ server/controllers/logoutController.js
@@ -1,6 +1,8 @@
+const { cookieSettings } = require("../config");
+
 async function logoutUser(req, res) {
   try {
-    res.clearCookie("token", { httpOnly: true });
+    res.clearCookie("token", cookieSettings);
     res.send({ ok: true });
   } catch(err) {
     res.status(500).send(err);
